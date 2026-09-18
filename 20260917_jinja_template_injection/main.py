45c622c7579caa80971c46203046f15e660069f9
jonghyuk.park <whdrns2013@naver.com>
2026-09-18 13:12:48 +0900

    [docs] jinja template injection example writing

--- 20260917_jinja_template_injection/main.py
+++ 20260917_jinja_template_injection/main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from jinja2 import Template
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+class User:
+    def __init__(self):
+        self.name   : str = "Alice"
+        self.secret : str = "Alice's Secret"
+
+user = User()
+
+# @app.get("/")
+# def home(name: str):
+#     global user
+#     template = Template(f"""
+#     <html>
+#       <body>
+#         Hello {name}
+#       </body>
+#     </html>                    
+#     """)
+#     return HTMLResponse(template.render())
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        # context={"name": "Alice"},
-        context={"name": "{{10+20}}"},
-    )
+def home(name: str):
+    template = Template(name)
+    return HTMLResponse(template.render(user=user))
